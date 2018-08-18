0d5ab06af5d2222a822dcd6702eed0c9bdd3de9f
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2018-08-18 20:16:33 +0300

    Update exports

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,12 +1,2 @@
-import * as sm from "./SystemManager";
-import * as em from "./EntityManager";
-export declare namespace ECS {
-    export import SystemManager = sm.SystemManager;
-    export import ISystem = sm.ISystem;
-    export import EntityManager = em.EntityManager;
-    export import EEntityManagerEventTypes = em.EEntityManagerEventTypes;
-    export import IEntity = em.IEntity;
-    export import IEntityData = em.IEntityData;
-    export import TEntityManagerListenerFn = em.TEntityManagerListenerFn;
-}
-export default ECS;
+export { EntityManager, EEntityManagerEventTypes, IEntity, IEntityData, } from "./EntityManager";
+export { SystemManager, ISystem, } from "./SystemManager";
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
-import * as sm from "./SystemManager";
-import * as em from "./EntityManager";
+export {
+    EntityManager,
+    EEntityManagerEventTypes,
+    IEntity,
+    IEntityData,
+} from "./EntityManager";
 
-export namespace ECS {
-    export import SystemManager = sm.SystemManager;
-    export import ISystem = sm.ISystem;
-
-    export import EntityManager = em.EntityManager;
-    export import EEntityManagerEventTypes = em.EEntityManagerEventTypes;
-    export import IEntity = em.IEntity;
-    export import IEntityData = em.IEntityData;
-    export import TEntityManagerListenerFn = em.TEntityManagerListenerFn;
-}
-
-export default ECS;
+export {
+    SystemManager,
+    ISystem,
+} from "./SystemManager";
