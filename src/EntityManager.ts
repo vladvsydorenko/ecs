6f073f8050ab40ea23f80c95a5822fc5b5be3cb1
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2018-08-22 22:32:07 +0300

    Add branches

--- src/EntityManager.ts
+++ src/EntityManager.ts
@@ -1,40 +1,60 @@
 import { EntityList } from "./EntityList";
+import { TEventListener, EventEmitter } from "./EventEmitter";
 
-interface IEntityManagerGroup<T> {
+interface IEntityManagerGroupTransform<T> {
     id: string;
     transform: TEntityManagerTransformFn<T, any>;
 }
 
 export interface IEntityManagerOptions {
     idKey?: string;
-    noDefaultGroups?: boolean;
+    parent?: EntityManager<any>;
+    groups?: {
+        [groupId: string]: TEntityManagerTransformFn<any>;
+    };
+}
+
+export interface IEntityManagerBranchOptions {
+    groups?: {
+        [groupId: string]: TEntityManagerTransformFn<any>;
+    };
 }
 
 export type TEntityManagerTransformFn<T, R = T> = (entity: T) => boolean | R;
 
 export interface IEntityManagerGroups { [groupId: string]: EntityList<any>; }
 
+export enum EEntityManagerEventTypes {
+    set = "set",
+    unset = "unset",
+}
+
 export class EntityManager<
     T,
     T_Groups extends IEntityManagerGroups = IEntityManagerGroups
 > {
-    public length = 0;
     public groups: Readonly<T_Groups> = Object.freeze({} as any);
 
     private idKey: string;
     private groupEntityLists: EntityList<EntityList>;
-    private groupTransforms: EntityList<IEntityManagerGroup<T>>;
+    private groupTransforms: EntityList<IEntityManagerGroupTransform<T>>;
+
+    private parent: EntityManager<any>;
+    private parentListenerIds: number[] = [];
+
+    private eventEmitter = new EventEmitter();
 
     constructor(options: IEntityManagerOptions = {}) {
         this.idKey = options.idKey || "id";
 
         this.groupEntityLists = new EntityList<EntityList>({ idKey: "id" });
-        this.groupTransforms = new EntityList<IEntityManagerGroup<T>>({ idKey: "id", });
+        this.groupTransforms = new EntityList<IEntityManagerGroupTransform<T>>({ idKey: "id", });
 
-        if (!options.noDefaultGroups) this.addGroup("all", () => true);
+        if (options.parent) this.setParent(options.parent);
+        if (options.groups) this.groupMany(options.groups);
     }
 
-    public addGroup<R>(id: string, transform: TEntityManagerTransformFn<T, R>): void {
+    public group<R = T>(id: string, transform: TEntityManagerTransformFn<T, R>): void {
         this.groupTransforms.set({
             id,
             transform,
@@ -48,33 +68,92 @@ export class EntityManager<
         };
     }
 
-    public removeGroup<R>(id: string): void {
+    public ungroup<R>(id: string): void {
         this.groupTransforms.unset(id);
     }
 
-    public getGroups(): IEntityManagerGroups {
+    public getGroups(): Readonly<IEntityManagerGroups> {
         return this.groupEntityLists.toObject();
     }
 
+    public groupMany(groups: IEntityManagerOptions["groups"]) {
+        Object
+            .keys(groups)
+            .forEach(groupId => {
+                this.group(groupId, groups[groupId]);
+            }, this);
+    }
+
     public set(data: T): void {
+        if (this.parent) this.parent.set(data);
+        else this.setLocal(data);
+    }
+
+    public unset(data: T | string): void {
+        if (this.parent) this.parent.unset(data);
+        else this.unsetLocal(data);
+    }
+
+    public setLocal(data: T): void {
+        let isSet = false;
+        let isUnset = false;
         this.groupTransforms.toArray().forEach(({ id, transform }) => {
             const result = transform(data);
             const group = this.groupEntityLists.get(id);
 
             if (result === false) {
                 group.unset(data);
+                isUnset = !isSet;
                 return;
             }
 
             const entity = result === true ? data : result;
             group.set(entity);
+            isSet = true;
+            isUnset = false;
         });
+
+        if (isSet) this.eventEmitter.emit(EEntityManagerEventTypes.set, data);
+        else if (isUnset) this.eventEmitter.emit(EEntityManagerEventTypes.unset, data);
     }
 
-    public unset(data: T | string): void {
-        this.groupTransforms.toArray().forEach(({ id, transform }) => {
+    public unsetLocal(data: T | string): void {
+        this.groupTransforms.toArray().forEach(({ id }) => {
             const group = this.groupEntityLists.get(id);
             group.unset(data);
+        });
+
+        this.eventEmitter.emit(EEntityManagerEventTypes.unset, data);
+    }
+
+    public setParent(parent: EntityManager<any>): void {
+        this.parent = parent;
+        this.parentListenerIds = [
+            parent.on(EEntityManagerEventTypes.set, this.setLocal, this),
+            parent.on(EEntityManagerEventTypes.unset, this.unsetLocal, this)
+        ];
+    }
+
+    public unsetParent(): void {
+        if (!this.parent) return;
+
+        this.parentListenerIds.forEach(this.parent.off, this.parent);
+        this.parent = undefined;
+    }
+
+    public on(eventId: string, listener: TEventListener<T>, thisArg?: any): number {
+        return this.eventEmitter.on(eventId, listener, thisArg);
+    }
+
+    public off(listenerId: number): void {
+        this.eventEmitter.off(listenerId);
+    }
+
+    public branch(options: IEntityManagerOptions) {
+        return new EntityManager({
+            parent: this,
+            idKey: this.idKey,
+            groups: options.groups,
         });
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,37 @@
 import { EntityManager } from "./EntityManager";
-import { EntityList } from "./EntityList";
 
-const em = new EntityManager<any>({ idKey: "id", noDefaultGroups: true });
-
-em.addGroup("test", entity => {
-    return typeof entity.name === "string"
+const em = new EntityManager<any>({ idKey: "id", groups: {
+    "all": () => true,
+} });
+const em2 = em.branch({
+    groups: {
+        "users": entity => typeof entity.name === "string",
+    }
+});
+const em3 = em2.branch({
+    groups: {
+        "users": entity => (
+            typeof entity.name === "string" &&
+            typeof entity.password === "string"
+        ),
+    }
 });
 
 em.set({
     id: "1",
-    name: "Hi"
+    name: "Test User",
 });
 
-// em.set({
-//     id: "1"
-// });
+em3.set({
+    id: "2",
+    name: "Another Test User",
+});
 
-console.log(em.groups);
+em2.set({
+    id: "3",
+    name: "Another Test User",
+    password: "superPassword",
+});
+
+console.log(em2.groups);
+console.log(em3.groups);
